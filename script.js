e60a15adda17a078b15c70e14fca76b27642fcd3
kaminiyuvaraju <kaminiyuvaraju@gmail.com>
2025-05-16 07:09:00 +0530

    🚀 Stunning portfolio added

---
+++ script.js
@@ -0,0 +1,41 @@
+// Typing effect
+const phrases = [
+  "Data Scientist",
+  "ML Engineer",
+  "Flask & FastAPI Developer",
+  "GATE DS & AI 2025 Qualified"
+];
+let i = 0, j = 0, currentPhrase = [], isDeleting = false;
+
+const typing = document.querySelector(".typing");
+
+function loop() {
+  typing.innerHTML = currentPhrase.join("");
+
+  if (!isDeleting && j <= phrases[i].length) {
+    currentPhrase.push(phrases[i][j]);
+    j++;
+  } else if (isDeleting && j > 0) {
+    currentPhrase.pop();
+    j--;
+  }
+
+  if (j === phrases[i].length) {
+    isDeleting = true;
+  }
+
+  if (isDeleting && j === 0) {
+    currentPhrase = [];
+    isDeleting = false;
+    i = (i + 1) % phrases.length;
+  }
+
+  setTimeout(loop, isDeleting ? 70 : 120);
+}
+
+loop();
+
+// Dark Mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
